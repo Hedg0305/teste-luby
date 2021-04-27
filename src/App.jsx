e3dc2573283227fdffccf9e1d10231c8360b0584
Henrique Garcia <hd.garcia@inf.ufpel.edu.br>
2021-04-27 12:51:48 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter } from 'react-router-dom';
 
 import { UserContextProvider } from './contexts/UserContext';
 import Bio from './pages/Bio';
+import Repos from './pages/Repos';
 import  Searching  from './pages/Searching';
 
 import './styles/global.scss';
@@ -14,6 +15,7 @@ const App = () => {
         <BrowserRouter>
             <Route path="/" exact component={Searching}/>
             <Route path="/bio" component={Bio}/>
+            <Route path="/repos" component={Repos}/>
         </BrowserRouter>
       </UserContextProvider>
     </div>
