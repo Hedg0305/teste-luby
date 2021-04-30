62046baa8364435cfe0c313b6c78eb8a986a3df3
Henrique Garcia <hd.garcia@inf.ufpel.edu.br>
2021-04-30 14:20:07 -0300

    Final version

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { HiArrowLeft } from 'react-icons/hi';
 import styles from './style.module.scss';
 
 const Header = ({ number, title }) => {
-  const history = useHistory();
-
-  const handleClick = () => {
-    history.goBack();
-  };
+  const { id } = useParams();
 
   return (
     <div className={styles.header}>
-      <button type='button' onClick={handleClick}>
+      <Link to={`/${id}`}>
         <HiArrowLeft size={26} />
-      </button>
+      </Link>
       <p>{`${number} ${title}`}</p>
       <div />
     </div>
